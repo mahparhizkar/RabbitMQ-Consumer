dc2b6baeadc538b7e40d0ad574a53c35d9dd1e19
m.parhizkar <mah.parhizkar88@gmail.com>
2021-07-23 12:51:46 +0430

    RabbitMQ Consumer

---
+++ src/main/java/com/example/demo/utils/Const.java
@@ -0,0 +1,9 @@
+package com.example.demo.utils;
+
+public class Const {
+
+    public static final String MARKETING_QUEUE = "marketingQueue";
+    public static final String FINANCE_QUEUE = "financeQueue";
+    public static final String ADMIN_QUEUE = "adminQueue";
+    public static final String ALL_QUEUE = "allQueue";
+}
